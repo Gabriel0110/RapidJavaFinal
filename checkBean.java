93ee8a47027a2b6d15737eccf06de77b74d0d0dc
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2018-04-18 16:30:03 -0400

    Update checkBean.java

--- checkBean.java
+++ checkBean.java
@@ -6,10 +6,8 @@
 package project02;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +16,12 @@ import java.util.Scanner;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.event.ValueChangeEvent;
 import javax.inject.Named;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  *
@@ -27,6 +31,7 @@ import javax.inject.Named;
 @ApplicationScoped
 public class checkBean implements Serializable {
 
+    String content;
     String textArea;
     String resultBox;
     String description;
@@ -35,13 +40,14 @@ public class checkBean implements Serializable {
     List<String> chapterList;
     List<String> exerciseList;
     Object newValue;
-    boolean hasInput;
     boolean isOutput;
+    boolean isInput;
     HashMap<String, String> inputCheck;
     final static int EXECUTION_TIME_ALLOWED = 1000;
     final static int EXECUTION_TIME_INTERVAL = 100;
-    
+
     static class Output {
+
         public String output = "";
         public String error = "";
         public boolean isInfiniteLoop = false;
@@ -55,7 +61,7 @@ public class checkBean implements Serializable {
 
     public void initialize() throws FileNotFoundException, IOException {
         chapterList = new ArrayList<>();
-        hasInput = false;
+        isInput = false;
         isOutput = false;
         BufferedReader reader = null;
         try {
@@ -101,14 +107,6 @@ public class checkBean implements Serializable {
     public void setExercise(String exercise) {
         this.exercise = exercise;
     }
-    
-    public boolean getHasInput() {
-        return hasInput;
-    }
-    
-    public void setHasInput(boolean hasInput) {
-        this.hasInput = hasInput;
-    }
 
     public List<String> getExerciseList() {
         return exerciseList;
@@ -130,43 +128,43 @@ public class checkBean implements Serializable {
             }
         }
     }
-    
+
     public void changeAreaListener(ValueChangeEvent event) {
         textArea = event.getNewValue().toString();
         setTextArea(textArea);
     }
-    
+
     public void changeNameListener(ValueChangeEvent event) {
         chapter = event.getNewValue().toString();
         setExerciseList(chapter);
     }
-    
+
     public void changeExerciseListener(ValueChangeEvent event) {
         if (!"Exercise01_01".equals(exercise)) {
             exercise = event.getNewValue().toString();
             setExercise(exercise);
         }
-        
+
     }
-    
+
     public String getDescription() {
         setExercise(exercise);
-        String ch = chapter.replaceAll("[a-zA-Z\\s]","");
-        String ex = exercise.substring(exercise.length() - 2, exercise.length()).replaceAll("[a-zA-Z\\s]","");
-        
+        String ch = chapter.replaceAll("[a-zA-Z\\s]", "");
+        String ex = exercise.substring(exercise.length() - 2, exercise.length()).replaceAll("[a-zA-Z\\s]", "");
+
         if (exercise.contains("Extra")) {
-            ex = exercise.substring(exercise.length() - 7, exercise.length() - 5).replaceAll("[a-zA-Z\\s]","");
+            ex = exercise.substring(exercise.length() - 7, exercise.length() - 5).replaceAll("[a-zA-Z\\s]", "");
         }
-        
-        if(exercise == null || "Exercise01_01".equals(exercise)){
+
+        if (exercise == null || "Exercise01_01".equals(exercise)) {
             return "/* Paste your Exercise01_01 here and click Automatic Check.\n "
-                        + "For all programming projects, the numbers should be double\n "
-                        + "unless it is explicitly stated as integer.\n "
-                        + "If you get a java.util.InputMismatchException error, check if\n "
-                        + "your code used input.readInt(), but it should be input.readDouble().\n "
-                        + "For integers, use int unless it is explicitly stated as long. */ ";
+                    + "For all programming projects, the numbers should be double\n "
+                    + "unless it is explicitly stated as integer.\n "
+                    + "If you get a java.util.InputMismatchException error, check if\n "
+                    + "your code used input.readInt(), but it should be input.readDouble().\n "
+                    + "For integers, use int unless it is explicitly stated as long. */ ";
         }
-       
+
         if (Integer.parseInt(ch) < 10) {
             if (Integer.parseInt(ex) < 10) {
                 return "/* Paste your " + getExercise() + " here and click Automatic Check.\n "
@@ -176,21 +174,21 @@ public class checkBean implements Serializable {
                         + "your code used input.readInt(), but it should be input.readDouble().\n "
                         + "For integers, use int unless it is explicitly stated as long. */ ";
             }
-             return "/* Paste your " + getExercise() + " here and click Automatic Check.\n "
-                        + "For all programming projects, the numbers should be double\n "
-                        + "unless it is explicitly stated as integer.\n "
-                        + "If you get a java.util.InputMismatchException error, check if\n "
-                        + "your code used input.readInt(), but it should be input.readDouble().\n "
-                        + "For integers, use int unless it is explicitly stated as long. */ ";
+            return "/* Paste your " + getExercise() + " here and click Automatic Check.\n "
+                    + "For all programming projects, the numbers should be double\n "
+                    + "unless it is explicitly stated as integer.\n "
+                    + "If you get a java.util.InputMismatchException error, check if\n "
+                    + "your code used input.readInt(), but it should be input.readDouble().\n "
+                    + "For integers, use int unless it is explicitly stated as long. */ ";
         }
         if (Integer.parseInt(ex) < 10) {
-                return "/* Paste your " + getExercise() + " here and click Automatic Check.\n "
-                        + "For all programming projects, the numbers should be double\n "
-                        + "unless it is explicitly stated as integer.\n "
-                        + "If you get a java.util.InputMismatchException error, check if\n "
-                        + "your code used input.readInt(), but it should be input.readDouble().\n "
-                        + "For integers, use int unless it is explicitly stated as long. */ ";
-            }
+            return "/* Paste your " + getExercise() + " here and click Automatic Check.\n "
+                    + "For all programming projects, the numbers should be double\n "
+                    + "unless it is explicitly stated as integer.\n "
+                    + "If you get a java.util.InputMismatchException error, check if\n "
+                    + "your code used input.readInt(), but it should be input.readDouble().\n "
+                    + "For integers, use int unless it is explicitly stated as long. */ ";
+        }
         return "/* Paste your Exercise01_01 here and click Automatic Check.\n "
                 + "For all programming projects, the numbers should be double\n "
                 + "unless it is explicitly stated as integer.\n "
@@ -202,7 +200,7 @@ public class checkBean implements Serializable {
     public void setDescription(String description) {
         this.description = description;
     }
-    
+
     public static Output executeProgram(String command, String program,
             String programDirectory, String inputFile, String outputFile) {
         final Output result = new Output();
@@ -264,7 +262,7 @@ public class checkBean implements Serializable {
             ex.printStackTrace();
         }
         result.timeUsed = (int) (System.currentTimeMillis() - startTime);
-        
+
         return result;
     }
 
@@ -332,16 +330,23 @@ public class checkBean implements Serializable {
         return result;
     }
 
-    public void compile() {
+    public void compile() throws IOException {
+        createFile();
+        File str = new File("C:\\book\\" + exercise + ".class");
+
         Output output = compileProgram("javac",
-          "C:\\book", exercise + ".java");
+                "C:\\book", exercise + ".java");
         System.out.println(output.error);
         System.out.println("Result: " + output.output);
 
-        // RUN WITHOUT INPUT //
-        output = executeProgram("java", exercise,
-                "C:\\book", null, "C:\\book\\" + exercise + ".output");
-        
+        if (str.exists()) {
+            // RUN WITHOUT INPUT //
+            output = executeProgram("java", exercise,
+                    "C:\\book", null, "C:\\book\\" + exercise + ".output");
+        } else {
+            setTextArea(output.error);
+        }
+
         // RUN WITH INPUT //
 //        output = executeProgram("java", exercise,
 //                "C:\\book", inputFromInputBox,
@@ -350,62 +355,94 @@ public class checkBean implements Serializable {
 //        System.out.println(output.error);
 //
 //        System.out.println("Result: " + output.output);
-        
         // now you would get the contents of the output file and send it to the textarea for displaying
     }
-    
+
+    public void createFile() throws IOException {
+        //Files.delete(C:\\book\\" + exercise + ".java);
+
+        File file = new File("C:\\book\\" + exercise + ".java");
+        FileWriter fileWriter = new FileWriter(file);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.print(textArea);
+        printWriter.close();
+    }
+
+    static String readFile(String path)
+            throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded);
+    }
+
     public boolean getIsOutput() {
         return isOutput;
     }
-    
+
     public void setIsOutput(boolean isOutput) {
         this.isOutput = isOutput;
     }
-    
+
     public void hasOutput() {
         isOutput = true;
     }
-    
+
     public void hasNoOutput() {
         isOutput = false;
     }
-    
+
+    public boolean getIsInput() {
+        return isInput;
+    }
+
+    public void setIsInput(boolean isInput) {
+        this.isInput = isInput;
+    }
+
+    public void hasInput() {
+        File dir = new File("C:\\Users\\gabri\\Documents\\NetBeansProjects\\Tomberlin\\web\\gradeexercise");
+        File[] directoryListing = dir.listFiles();
+
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                String name = child.getName();
+                if (name.contains(exercise) && name.contains(".input")) {
+                    isInput = true;
+                }
+
+            }
+        }
+    }
+
     public String getResultBox() throws IOException {
         return "<div id=\"editorForResult\" style=\"text-align: left; width:575px; height:181px; font-weight: normal; font-size: 96%; color: black; background-color: white; border: 1px solid; border-color: #f6912f;height:147px;; width:100%; margin: 0 auto;\">"
-+ "command&gt;javac " + exercise + ".java\n" +
-"Compiled successful\n" +
-"\n" +
-"command&gt;java " + exercise + "\n" + textArea +
-"\n" +
-"command&gt;\n" +
-"</div>";
+                + "command&gt;javac " + exercise + ".java\n"
+                + "Compiled successful\n"
+                + "\n"
+                + "command&gt;java " + exercise + "\n" + textArea
+                + "\n"
+                + "command&gt;\n"
+                + "</div>";
     }
-    
+
+    public String getInputBox() throws IOException {
+        return "<textarea id=\"dataInputTextarea\" name=\"jsfForm:dataInputTextarea\" style=\"font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif, monospace !important; width:575px; height:40px; background-color: white; border: 1px solid #f6912f; font-weight: normal; font-size: 96%; margin-top: 0px; width:100%; margin: 0 auto;\">" + getInputText() + "</textarea>";
+    }
+
     public String getTextArea() {
         return textArea;
     }
-    
+
     public void setTextArea(String textArea) {
         this.textArea = textArea;
     }
-    
-    
-    
-//    public List getResults() throws FileNotFoundException, IOException {
-//        List<String> results = new ArrayList<>();
-//        
-//        BufferedReader reader = null;
-//        try {
-//            reader = new BufferedReader(new FileReader("C:\\book\\" + exercise + ".output"));
-//            String line = null;
-//            while ((line = reader.readLine()) != null) {
-//                results.add(line);
-//            }
-//        } finally {
-//            reader.close();
-//        }
-//        
-//        return results;
-//    }
+
+    public String getLabel() {
+        return "<label style=\"font-weight: bold; font-family: times; width:100%; margin: 0 auto;\">Enter input data for the program (Sample data provided below. You may modify it.)</label>";
+    }
+
+    public String getInputText() throws FileNotFoundException, IOException {
+        String str = readFile("C:\\Users\\gabri\\Documents\\NetBeansProjects\\Tomberlin\\web\\gradeexercise\\" + exercise + "a.input");
+        return str;
+    }
 
 }
